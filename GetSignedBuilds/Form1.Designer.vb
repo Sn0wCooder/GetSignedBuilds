82d94da9f225fab45b019fd14f1120e8076c51aa
Leonardo Alfreducci <leoalfreducci@gmail.com>
2016-04-12 19:54:43 +0200

    First commit

---
+++ GetSignedBuilds/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Device:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(62, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.Label2.Location = New System.Drawing.Point(86, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(25, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "e.g."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(107, 35)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "iPhone4,1"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 54)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(228, 100)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Signed builds:"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(6, 19)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(216, 69)
+        Me.ListBox1.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(168, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 38)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Get signed builds"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(122, 157)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(121, 13)
+        Me.LinkLabel2.TabIndex = 9
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "Open Source on GitHub"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(249, 177)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "GetSignedBuilds v1.0"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents LinkLabel2 As System.Windows.Forms.LinkLabel
+
+End Class
---
+++ GetSignedBuilds/Form1.vb
@@ -0,0 +1,125 @@
+﻿Public Class Form1
+    Dim da As String = My.Computer.FileSystem.SpecialDirectories.CurrentUserApplicationData & "\"
+    Public signedios As String()
+    Public signeduris As String()
+    Dim WC As New System.Net.WebClient
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If CheckForInternetConnection() = True Then
+            If System.IO.File.Exists(da & "fws.json") Then
+                System.IO.File.Delete(da & "fws.json")
+            End If
+            WC.DownloadFile("https://api.ipsw.me/v2.1/firmwares.json", da & "fws.json")
+        Else
+            MsgBox("ERROR: this application require an internet connection to work.", MsgBoxStyle.Critical, "Error")
+        End If
+    End Sub
+    Public Function CheckForInternetConnection() As Boolean
+        Try
+            Using client = New System.Net.WebClient()
+                Using stream = client.OpenRead("http://www.google.com")
+                    Return True
+                End Using
+            End Using
+        Catch
+            Return False
+        End Try
+    End Function
+
+    Public Function GetSignedSHSHs(ByVal info As String)
+        If Not System.IO.File.Exists(da & "fws.json") Then
+            WC.DownloadFile("https://api.ipsw.me/v2.1/firmwares.json", da & "fws.json")
+        End If
+        ' I know there are better ways to read a Json file in vb :/
+
+        Dim Json() As String = IO.File.ReadAllLines(da & "fws.json")
+
+        Dim Jsonlines As Integer = Json.Length
+        Dim position As Integer = 0
+        Dim buildoffset As Integer = 0
+        Dim versoffset As Integer = 0
+        Dim urisoffset As Integer = 0
+        Dim rightdevice As Boolean = False
+
+        Dim signedbuilds As String() = {}
+        Dim signedios1 As String() = {}
+        Dim signeduris1 As String() = {}
+        Dim signedcount As Integer = -1
+
+        Dim open = 0
+        Dim closed = 0
+
+        Do While position <> Jsonlines
+
+            If Json(position).Contains(info) Then
+                rightdevice = True
+            End If
+
+            If Json(position).Contains("{") And rightdevice = True Then
+                open = open + 1
+            End If
+
+            If Json(position).Contains("}") And rightdevice = True Then
+                closed = closed + 1
+            End If
+
+            If open = closed And open <> 0 Then
+                rightdevice = False
+            End If
+
+            If Json(position).Contains("""" + "version" + """" + ": ") And rightdevice = True Then
+                versoffset = position
+            End If
+
+            If Json(position).Contains("""" + "buildid" + """" + ": ") And rightdevice = True Then
+                buildoffset = position
+            End If
+
+            If Json(position).Contains("""" + "url" + """" + ": ") And rightdevice = True Then
+                urisoffset = position
+            End If
+
+            If Json(position).Contains("true") And rightdevice = True Then
+                signedcount = signedcount + 1
+
+                Dim b = Json(versoffset).Replace("""" + "version" + """" + ": " + """", "").Replace("""" + ",", "").Trim()
+                Dim a = Json(buildoffset).Replace("""" + "buildid" + """" + ": " + """", "").Replace("""" + ",", "").Trim()
+                Dim c = Json(urisoffset).Replace("""" + "url" + """" + ": " + """", "").Replace("""" + ",", "").Trim()
+
+                ReDim Preserve signedbuilds(signedcount)
+                signedbuilds(signedcount) = a
+
+                ReDim Preserve signedios1(signedcount)
+                signedios1(signedcount) = b
+
+                ReDim Preserve signeduris1(signedcount)
+                signeduris1(signedcount) = c
+
+            End If
+
+            position = position + 1
+        Loop
+
+
+        signedios = signedios1
+        signeduris = signeduris1
+        Return signedbuilds
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim SignedBuilds As String() = GetSignedSHSHs(TextBox1.Text)
+        If SignedBuilds.Count = 0 Then
+            MsgBox("ERROR: device invalid!", MsgBoxStyle.Critical, "Error")
+        Else
+            ListBox1.Items.Clear()
+            GroupBox1.Text = "Signed builds for: " & TextBox1.Text
+            For Each builds In SignedBuilds
+                ListBox1.Items.Add(builds)
+            Next
+        End If
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Process.Start("https://github.com/Sn0wCooder/GetSignedBuilds")
+    End Sub
+End Class
